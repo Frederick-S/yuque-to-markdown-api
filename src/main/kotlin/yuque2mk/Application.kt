2f252973e681085c00d71f8b23fb4dbe036cc81e
Frederick-S <mao_xiaodan@hotmail.com>
2021-05-09 16:32:23 +0800

    yuque auth

--- src/main/kotlin/yuque2mk/Application.kt
+++ src/main/kotlin/yuque2mk/Application.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class Yuque2mkApplication
+class Application
 
 fun main(args: Array<String>) {
-	runApplication<Yuque2mkApplication>(*args)
+	runApplication<Application>(*args)
 }
